bae4f6c4995996716b34f9c1ef1fc11de3a42c89
Brian <bndamira@gmail.com>
2023-04-02 18:27:18 +0100

    change structure of hash_table of db keys

--- sstable.py
+++ sstable.py
@@ -10,9 +10,13 @@ class SSTable:
         self.database_dir = database_dir
         self.db_path = path.join(database_dir, 'database.csv')
 
+        self.hash_table = {}  # placeholder
+        self.no_char = 0 # placeholder
+        self.saved_segments = set()
+
         self.reset_cache()
         self.no_add_segmants = 0 # Number of additional segments to add to the database
-        self.seg_hashes = [] # stack of the previous segment hashes
+        self.key_seg_path = {} # 'key': ('seg_0.csv', 0) # key, (segment file name, char number)
 
 
         try:
@@ -46,13 +50,13 @@ class SSTable:
 
     def get_data_segment(self, key):
         """Look for the data in saved segments"""
-        for seg_filename, hash_tab in self.seg_hashes:
-            if key in hash_tab:
-                seg_path = path.join(self.database_dir, seg_filename)
-                with open(seg_path, 'r') as f:
-                    f.seek(hash_tab[key])
-                    line_val = f.readline()
-                return int(line_val.split(',')[1])
+        if key in self.key_seg_path:
+            seg_filename, char_no = self.key_seg_path[key] # TODO seg_hashes should give full path path to the key withouut need of looping
+            seg_path = path.join(self.database_dir, seg_filename)
+            with open(seg_path, 'r') as f:
+                f.seek(char_no)
+                line_val = f.readline()
+            return int(line_val.split(',')[1])
         return None
             
     def load_sstable(self):
@@ -70,12 +74,14 @@ class SSTable:
     def save_segment(self):
         """Separate database segment and start writing data to clean file. Used for data segment compation and merging """
         # 1, Create the copy of the file called new seg and save hash map for prevoius file
-        new_seg_name = f'seg_{self.no_add_segmants}.csv'
+        new_seg_name = self.new_segment_name()
         new_seg_path = path.join(self.database_dir, new_seg_name)
         self.no_add_segmants += 1
         shutil.copy(self.db_path, new_seg_path)
-        self.seg_hashes.insert(0,(new_seg_name, self.hash_table))
 
+        for key, char_no in self.hash_table.items():
+            self.key_seg_path[key] = (new_seg_name, char_no)
+        self.saved_segments.add(new_seg_name)
         # 2. Clean the curent file & Start new hash map for the file
         self.reset_cache()
     
@@ -90,4 +96,11 @@ class SSTable:
         self.hash_table = {}
         self.no_char = 0
 
-            
+    def new_segment_name(self):
+
+        new_name = f'seg_{self.no_add_segmants}.csv'
+        while new_name in self.saved_segments:
+            self.no_add_segmants += 1
+            new_name = f'seg_{self.no_add_segmants}.csv'
+
+        return new_name
